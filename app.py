2c4e1e83c1ee6cee091c14fdb8b8e5c317380d3c
Shubham <shubham@shubhams-MacBook-Pro.local>
2018-01-19 17:30:56 +0530

    Basic

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import requests
+import csv
+
+app = Flask(__name__)
+
+# @app.route("/sendMessage")
+# def sendMessage():
+# 	pass()
+
+
+def parseCsv():
+	with open("data.csv", "rb") as csvFile:
+		csvReader = csv.reader(csvFile, delimiter=',')
+		for row in csvReader:
+			print (row)
+	return "Success"
+
+@app.route("/run")
+def run():
+	r = requests.get('https://oyorooms.freshdesk.com/reports/scheduled_exports/6374371511853668/download_file.json', auth=('29ecd4SXzdbNYsmDJp0Z', 'X'))
+	csvURL = ((r.json())["export"])["url"]
+	csvData = requests.get(csvURL)
+	with open("data.csv", "w") as csvFile:
+		csvFile.write(csvData.content)
+	parseCsv()
